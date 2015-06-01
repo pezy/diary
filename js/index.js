dac0e71a6df684c74c202e70dcb6923d2cb53f1c
pezy <urbancpz@gmail.com>
2015-06-01 17:35:31 +0800

    Make diary notes parse from markdown directly.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-webpackJsonp([1],[function(t,e,n){var i=n(1),a=n(2);window.addEventListener("load",function(){var t=new i({onSelect:function(t){var e=a(t);window.open(e.format("YYYY/MM/YYYY-MM-DD")+".html","_blank")},i18n:{previousMonth:"&lt;&lt;",nextMonth:"&gt;&gt;",months:["January","February","March","April","May","June","July","August","September","October","November","December"],weekdays:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],weekdaysShort:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]},minDate:new Date("2015-04-23"),maxDate:new Date});document.getElementById("calendar").appendChild(t.el),document.querySelector(".pika-prev").textContent="<<",document.querySelector(".pika-next").textContent=">>"})}]);
+webpackJsonp([1], [function (t, e, n) {
+    var i = n(1), a = n(2);
+    window.addEventListener("load", function () {
+        var t = new i({
+            onSelect: function (t) {
+                var e = a(t);
+                window.open( "diary.html?path=" + e.format("YYYY/MM/YYYY-MM-DD") + "&title=" + e.format("Do.MMMM,YYYY"), "_blank")
+            },
+            i18n: {
+                previousMonth: "&lt;&lt;",
+                nextMonth: "&gt;&gt;",
+                months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                weekdays: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                weekdaysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+            },
+            minDate: new Date("2015-06-01"),
+            maxDate: new Date
+        });
+        document.getElementById("calendar").appendChild(t.el), document.querySelector(".pika-prev").textContent = "<<", document.querySelector(".pika-next").textContent = ">>"
+    })
+}]);
